840310f2555734f418eb6a4ee0f32f8c882d54a6
florien2208 <mahorompakanyiflorien@gmail.com>
2024-04-21 15:11:20 +0200

    updates

---
+++ src/app/components/HomeBanner.tsx
@@ -0,0 +1,16 @@
+const HomeBanner = () => {
+  return (
+    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700">
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center">
+        <div>
+            <h1>Summer Sales</h1>
+            <p>Enjoy discount on selected items</p>
+            <p>GET 50% OFF</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
