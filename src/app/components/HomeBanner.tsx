83fa86b19ffd05444a91f979dc734338ae73505b
florien2208 <mahorompakanyiflorien@gmail.com>
2024-04-23 17:52:39 +0200

    small updates

--- src/app/components/HomeBanner.tsx
+++ src/app/components/HomeBanner.tsx
@@ -1,13 +1,15 @@
+import Image from "next/image";
+
 const HomeBanner = () => {
   return (
     <div className="relative bg-gradient-to-r from-sky-500 to-sky-700">
-      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center">
-        <div>
-            <h1>Summer Sales</h1>
-            <p>Enjoy discount on selected items</p>
-            <p>GET 50% OFF</p>
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+        <div className="mb-8 md:mb-0 text-center">
+            <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Summer Sales</h1>
+            <p className="text-lg md:text-xl text-white mb-2 ">Enjoy discount on selected items</p>
+            <p className="text-2xl md:text-5xl text-yellow-400 font-bold">GET 50% OFF</p>
         </div>
-        <div></div>
+        <div className="w-1/3 relative aspect-video"><Image src="/catbanner-02.jpg" fill alt="banner image" className="object-contain"/></div>
       </div>
     </div>
   );
