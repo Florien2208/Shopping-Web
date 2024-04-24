4a7468032741adcffd30afd53dcb10fcc520a009
florien2208 <mahorompakanyiflorien@gmail.com>
2024-04-24 10:00:38 +0200

    small updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Container from "./components/Container";
-import products from "./utils/product"
+
 import HomeBanner from "./components/HomeBanner";
+import ProductsCard from "./components/products/ProductsCard";
+import { products } from "./utils/product";
 import { trancateText } from "./utils/trancateText";
 
 export default function Home() {
@@ -10,7 +12,7 @@ export default function Home() {
         <div><HomeBanner/></div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">{
           (products.map((product: any)=>{
-            return<div>{trancateText(product.name)}</div>
+            return <ProductsCard data={product}/>
           }))
           }</div>
       </Container>
